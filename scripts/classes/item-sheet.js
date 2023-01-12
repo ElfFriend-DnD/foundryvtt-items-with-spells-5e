d73912cdc9feea1ac88372c096e1bc21ab0942d1
Kenneth B <kennethzhell@gmail.com>
2023-01-12 21:15:15 +0100

    add settings menu to toggle optional item types.

--- scripts/classes/item-sheet.js
+++ scripts/classes/item-sheet.js
@@ -2,7 +2,6 @@
 import { ItemsWithSpells5e } from '../items-with-spells-5e.js';
 import { ItemsWithSpells5eItemSpellOverrides } from './item-spell-overrides.js';
 import { ItemsWithSpells5eItem } from './item.js';
-import { EXTRA_EXCLUDED_TYPES } from './settings.mjs';
 
 /**
  * A class made to make managing the operations for an Item sheet easier.
@@ -26,11 +25,11 @@ export class ItemsWithSpells5eItemSheet {
    */
   static init() {
     Hooks.on('renderItemSheet', (app, html) => {
-      if (app.item.type === 'spell') {
-        return;
-      }
-      const exclude = game.settings.get("items-with-spells-5e", "itemTypeExclusion");
-      if(exclude && EXTRA_EXCLUDED_TYPES.includes(app.item.type)) return;
+      let include = false;
+      try{
+        include = !!game.settings.get("items-with-spells-5e", `includeItemType${app.item.type.titleCase()}`);
+      }catch{}
+      if(!include) return;
 
       ItemsWithSpells5e.log(false, {
         instances: this.instances,
--- scripts/classes/settings.mjs
+++ scripts/classes/settings.mjs
@@ -1,19 +1,68 @@
-export const EXTRA_EXCLUDED_TYPES = [
+export const EXCLUDED_TYPES = [
   "class",
   "subclass",
-  "feat",
   "background",
-  "race"
+  "race",
+  "spell",
+  "loot"
 ];
 
 export function _registerSettings() {
-  game.settings.register("items-with-spells-5e", "itemTypeExclusion", {
+  const TYPES = Item.TYPES.filter(t => !EXCLUDED_TYPES.includes(t));
+
+  for(const type of TYPES){
+    game.settings.register("items-with-spells-5e", `includeItemType${type.titleCase()}`, {
+      scope: "world",
+      config: false,
+      type: Boolean,
+      default: true,
+      requiresReload: true
+    });
+  }
+
+  game.settings.registerMenu("items-with-spells-5e", "itemTypeExclusion", {
     name: "items-with-spells-5e.SETTINGS.ITEM_EXCLUSION.NAME",
     hint: "items-with-spells-5e.SETTINGS.ITEM_EXCLUSION.HINT",
     scope: "world",
     config: true,
-    type: Boolean,
-    default: false,
-    requiresReload: true
+    type: IWS_TypeSettings,
+    label: "items-with-spells-5e.SETTINGS.ITEM_EXCLUSION.NAME"
   });
+}
+
+class IWS_TypeSettings extends FormApplication {
+
+  get id(){
+    return "items-with-spells-5e-itemTypeExclusion-menu";
+  }
+
+  get title(){
+    return "Item Type Exclusion Menu";
+  }
+
+  get template(){
+    return "modules/items-with-spells-5e/templates/settingsMenu.hbs";
+  }
+
+  async getData(){
+    const TYPES = Item.TYPES.filter(t => !EXCLUDED_TYPES.includes(t));
+    const data = await super.getData();
+    data.types = [];
+    console.log({types: data.types, TYPES, data});
+    for(const type of TYPES){
+      const label = type.titleCase();
+      data.types.push({
+        checked: game.settings.get("items-with-spells-5e", `includeItemType${label}`),
+        value: type,
+        label
+      });
+    }
+    return data;
+  }
+
+  async _updateObject(event, formData){
+    Object.entries(formData).forEach(([type, bool]) => {
+      game.settings.set("items-with-spells-5e", `includeItemType${type.titleCase()}`, bool);
+    });
+  }
 }
